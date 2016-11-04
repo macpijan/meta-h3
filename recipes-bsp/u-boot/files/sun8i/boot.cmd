64607888bb667ad4747991958da6db6234b277b2
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2016-11-04 09:41:43 +0100

    u-boot/files/sun8i/boot.cmd: set resolution for 24" HDMI screen

--- recipes-bsp/u-boot/files/sun8i/boot.cmd
+++ recipes-bsp/u-boot/files/sun8i/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=${console} console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
+setenv bootargs earlyprintk=ttyS0,115200 loglevel=8 root=/dev/mmcblk0p2 rootwait rootfstype=ext4 video=HDMI-A-1:1920x1080-32@60 console=tty1 console=ttyS0,115200
 load mmc 0:1 ${fdt_addr_r} ${fdtfile}
 load mmc 0:1 ${kernel_addr_r} uImage
 bootm ${kernel_addr_r} - ${fdt_addr_r}
